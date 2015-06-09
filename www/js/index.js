f01d0f626186b6d074cfcb4adc6ade125dcf99ab
Haosheng Huang <haosheng.huang@tuwien.ac.at>
2015-06-09 21:22:00 +0200

    test (add circle)

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,15 @@
 function initialize() {
-    var map = L.map('map').setView([48.21,16.37], 13);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    var map = L.map('map').setView([48.209219, 16.370821], 16);
+    L.tileLayer('../../MapQuest_Vienna/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+	
+	var circle = L.circle([45.814910, 9.083399], 20, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+	}).addTo(map);
+	
 }
 
 document.addEventListener('deviceready', initialize);
