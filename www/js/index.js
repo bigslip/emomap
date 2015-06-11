256e0ef356c3e5134080054e745eb20bff8e6fb3
Haosheng Huang <haosheng.huang@tuwien.ac.at>
2015-06-11 17:53:36 +0200

    add all UI elements

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 function initialize() {
     var map = L.map('map').setView([48.209219, 16.370821], 16);
     L.tileLayer('../../tiles_vienna/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        //attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	
 			
@@ -10,6 +10,7 @@ function initialize() {
 		$("#start-menu").hide();
 		$("#slider-comfort").show();
 		$("#info").hide();
+		
 	});
 	
 	$("#start-menu-my").click(myEmoMap); //show the user' contributions
@@ -21,6 +22,10 @@ function initialize() {
 		$("#start-menu").show();
 		$("#slider-comfort").hide();
 		$("#info").hide();
+		$("#navbar-start").addClass("ui-btn-active");
+		$("#navbar-my").removeClass("ui-btn-active");
+		$("#navbar-all").removeClass("ui-btn-active");
+		$("#navbar-about").removeClass("ui-btn-active");
 	});
 	
 	$("#navbar-my").click(myEmoMap);	
@@ -29,10 +34,17 @@ function initialize() {
 	
 	$("#next").click(Next);
 	
-	
-	//get current location, we mock up a location
+	//initial values
 	var curLat = 48.209219;
 	var curLng = 16.370821;
+	var level_of_comfort=4;
+	var adj="";
+	var conx_with="alone";
+	var conx_first="first_time";
+	
+	//get current location, we need to use Geolocation
+	curLat = 48.209219;
+	curLng = 16.370821;
 	var locationIcon = L.icon({
 		iconUrl: 'css/lib/images/marker-icon.png'		
 	});
@@ -40,12 +52,46 @@ function initialize() {
 	marker.bindPopup("Drag the marker to correct location").openPopup();	
 	marker.on('dragend', function(event) {
 		var latlng = event.target.getLatLng();  
+		console.log(latlng);
 		curLat= latlng.lat;
 		curLng = latlng.lng;
+	});	
+	
+	//get level of comfort
+	$("#slider1").bind("slidestop", function () {  			
+		level_of_comfort = $("#slider1").val();
+		console.log(level_of_comfort);
 	});
 	
+	//get adjectives
+	$("#adj").bind( "change", function() {
+		adj = $(this).val();
+		console.log(adj);
+	});
 	
-	
+	//get context (with whom)
+	$(".checkbox-with").bind( "change", function() {
+		var click_alone=$(this).prop("value").includes("alone");			
+		if(click_alone){
+			$("#checkbox-h-2a").attr("checked",true).checkboxradio("refresh");
+			$("#checkbox-h-2b").attr("checked",false).checkboxradio("refresh");
+			$("#checkbox-h-2c").attr("checked",false).checkboxradio("refresh");
+			$("#checkbox-h-2d").attr("checked",false).checkboxradio("refresh");
+		}
+		else{
+			$("#checkbox-h-2a").attr("checked",false).checkboxradio("refresh");
+		}
+		
+		conx_with= $(".checkbox-with:checked").map(function () {return this.value;}).get().join(",");
+		
+		console.log(conx_with);
+	});
+	//get context (first time)
+	$("input[type='radio']").bind( "change", function() {
+        conx_first=$(this).val();
+		console.log(conx_first);
+	});
+
 }
 
 
@@ -54,6 +100,10 @@ function myEmoMap(){
 	$("#start-menu").hide();
 	$("#slider-comfort").hide();
 	$("#info").hide();
+	$("#navbar-start").removeClass("ui-btn-active");
+	$("#navbar-my").addClass("ui-btn-active");
+	$("#navbar-all").removeClass("ui-btn-active");
+	$("#navbar-about").removeClass("ui-btn-active");
 }
 
 //show all contributions
@@ -61,6 +111,10 @@ function allEmoMap(){
 	$("#start-menu").hide();
 	$("#slider-comfort").hide();
 	$("#info").hide();
+	$("#navbar-start").removeClass("ui-btn-active");
+	$("#navbar-my").removeClass("ui-btn-active");
+	$("#navbar-all").addClass("ui-btn-active");
+	$("#navbar-about").removeClass("ui-btn-active");
 }
 
 //information about emomap
@@ -68,6 +122,10 @@ function Info(){
 	$("#start-menu").hide();
 	$("#slider-comfort").hide();
 	$("#info").show();
+	$("#navbar-start").removeClass("ui-btn-active");
+	$("#navbar-my").removeClass("ui-btn-active");
+	$("#navbar-all").removeClass("ui-btn-active");
+	$("#navbar-about").addClass("ui-btn-active");
 }
 
 //next
